9be64afe796ec7156c4386a1ce806da7f3a1b03e
rodox-karius <rcarius@gmail.com>
2022-10-20 16:32:19 -0300

    Repositorios criados

---
+++ E-commerce/src/main/java/br/com/residencia/ecommerce/repository/Item_pedidoRepository.java
@@ -0,0 +1,9 @@
+package br.com.residencia.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.residencia.ecommerce.entity.Item_pedido;
+
+public interface Item_pedidoRepository extends JpaRepository<Item_pedido, Integer>{
+
+}
